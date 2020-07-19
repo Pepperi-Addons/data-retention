e9c0ff7b1f5882af48ffd299a4ba766ad62bbe09
roiaharon-pepperi <66308169+roiaharon-pepperi@users.noreply.github.com>
2020-07-19 16:21:59 +0300

    Added pepperi-menu

---
+++ client-side/src/app/components/pepperi-menu/pepperi-menu.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, Input, Output, OnChanges, SimpleChanges, EventEmitter } from '@angular/core';
+
+// @ts-ignore
+import { PepperiMenuComponent as PepperiMenuComp } from 'pepperi-menu';
+
+@Component({
+  selector: 'pepperi-menu',
+  template: `<ndc-dynamic #comp
+  [ndcDynamicComponent]="component"
+  [ndcDynamicInputs]="inputs"
+  [ndcDynamicOutputs]="outputs">
+</ndc-dynamic>`,
+  styles: []
+})
+export class PepperiMenuComponent implements OnInit, OnChanges {
+
+  constructor() { }
+
+  component = PepperiMenuComp
+  inputs = {}
+  outputs = {
+    notifyMenuItemClicked: (event: any) => {
+      this.menuItemClicked.emit(event);
+    }
+  }
+
+  ngOnInit(): void {
+    this.setInputs()
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.setInputs()
+  }
+
+  setInputs() {
+    //const value = this.options.find(item => item.Key === this.value);
+    this.inputs = {
+      key: '',
+      label: this.label,
+      options: this.options,
+      disabled: this.disabled,
+    }
+  }
+
+  @Output()
+  menuItemClicked = new EventEmitter<any>();
+
+  @Input()
+  options: any = {}
+
+  @Input()
+  label: string = ''
+
+  @Input()
+  disabled: boolean = false
+};
