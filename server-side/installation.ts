76e56b5f29bf5a4b3fbca9012d975d8e9454b603
roiaharon-pepperi <roi.a@pepperi.com>
2022-04-10 10:25:59 +0300

    DI-19889
    Can't install data retention add-on on new distributor

--- server-side/installation.ts
+++ server-side/installation.ts
@@ -32,7 +32,7 @@ exports.install = async (Client, Request) => {
         baseURL: Client.BaseURL,
         token: Client.OAuthAccessToken,
         addonUUID: Client.AddonUUID,
-        addonSecretKey: '8c9a5568-f35a-407d-856a-32d25ace2859'
+        addonSecretKey: Client.AddonSecretKey
     });
     try {
         let retVal = await createADALScheme(papiClient);
